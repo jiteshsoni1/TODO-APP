340d993be370b4ba021d9ebda39085c8b82fa68c
jiteshsoni1 <jiteshsoni3591@gmail.com>
2026-02-10 17:36:56 +0530

    new concept use

--- app.js
+++ app.js
@@ -2,30 +2,37 @@ let todo = [];
 
 let req = prompt("please enter your request");
 
-while(true) {
+while (true) {
   if (req == "quit") {
-    console.log("quiting app");
+    console.log("Quiting app");
     break;
   }
   if (req == "list") {
     console.log("-------");
-for(let i=0; i<todo.length; i++){
-    console.log(i,todo[i]);
-    
-}
+    for (let i = 0; i < todo.length; i++) {
+      console.log(i, todo[i]);
+    }
     console.log("-------");
   } else if (req == "add") {
-    let task = prompt("please enter the task you want to add");
+    let task = prompt("Please enter the task you want to add");
     todo.push(task);
-    console.log("task added");
-  } else if(req == "delete"){
-    let idx = prompt("please enter the task index");
+    console.log("Task added");
+  } else if (req == "delete") {
+    let idx = prompt("Please enter the task index");
     todo.splice(idx, 1);
-    console.log("task deleted");
+    console.log("Task deleted");
+    }else if(req === "new"){
+    let index = prompt("Enter index to update");
+    let newTask = prompt("Enter new task");
+    todo[index] = newTask;
+    console.log("Updated");
+}else if(req ==="edit"){
+    let index = prompt("Enter index to edit");
+    let newTask = prompt("Enter new task");
+    todo[index] = newTask;
+    console.log("Edited");
   } else {
-    console.log("wrong request");
+    console.log("Wrong request");
   }
-  req = prompt("please enter your request");
+  req = prompt("Please enter your request");
 }
-   
-q
